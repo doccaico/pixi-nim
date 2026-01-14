f1c6f08926523c3b247b7fe03500471efdb9e459
doccaico <hash_ext_luna@yahoo.co.jp>
2026-01-14 17:46:13 +0900

    up

--- pixi/pixi/assets.nim
+++ pixi/pixi/assets.nim
@@ -2,18 +2,11 @@ import std / [jsffi, asyncjs]
 
 var Assets* {.importc, nodecl.}: JsObject
 
-# proc assetsLoad*(_: cstring): Future[JsObject] {.importjs: "PIXI.Assets.load(#)".}
-# proc assetsLoad*(_: openArray[cstring]): Future[JsObject] {.importjs: "PIXI.Assets.load(#)".}
-
 proc load*(_: JsObject, _: cstring): Future[JsObject] {.importjs: "PIXI.#.load(#)".}
 proc load*(_: JsObject, _: openArray[cstring]): Future[JsObject] {.importjs: "PIXI.#.load(#)".}
 
 proc backgroundLoad*(_: JsObject, _: openArray[cstring]) {.importjs: "PIXI.#.backgroundLoad(#)".}
-# proc load*(_: JsObject, _: cstring): JsObject {.importjs: "PIXI.#.load(#)".}
 
 proc add*(_, _: JsObject) {.importjs: "PIXI.#.add(#)".}
 
-# proc then*(_: Future[JsObject], _: proc) {.importjs: "#.then(@)".}
-# proc then*(_: JsObject, _: proc) {.importjs: "#.then(@)".}
-# proc then*(_: Future[JsObject], _: proc(texture: Future[JsObject])) {.importjs: "#.then(@)".}
-proc then*(a: Future[JsObject], _: proc(a: JsObject)) {.importjs: "#.then(@)".}
+proc then*(a: Future[JsObject], _: proc(a: JsObject)) {.importjs: "#.then(#)".}
